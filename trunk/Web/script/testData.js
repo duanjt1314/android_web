e84be1a75c4725cf6df54baa23d9f39950c75393
duanjt1314 <479224006@qq.com>
2019-08-14 13:48:11 +0800

    生活费新增页面，修改为vue

---
+++ trunk/Web/script/testData.js
@@ -0,0 +1,32 @@
+﻿function request(methodName) {
+    if (methodName == 'diction_list') {
+        return {
+            success: true,
+            msg: "",
+            data: [{
+                id: 1,
+                name: "生活费"
+            }, {
+                id: 2,
+                name: "医药费"
+            }, {
+                id: 3,
+                name: "车费"
+            }]
+        };
+    } else if (methodName == "lifing_save") {
+        return {
+            success: true,
+            msg: "成功"
+        };
+    }
+}
+
+var contact = {
+    alertMsg: function (msg) {
+        alert(msg);
+    },
+    showMsg: function (msg) {
+        alert(msg);
+    }
+}
